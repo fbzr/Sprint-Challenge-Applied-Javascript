6f896af3ed5c74fb050c7f75de2e948931dc9d69
fbzr <fabricio.spb@gmail.com>
2020-02-14 09:22:33 -0800

    create images array

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -13,18 +13,17 @@ function Carousel() {
   const carousel = document.createElement('div');
   const leftButton = document.createElement('div');
 
-  const image1 = document.createElement('img');
-  const image2 = document.createElement('img');
-  const image3 = document.createElement('img');
-  const image4 = document.createElement('img');
+  const images = [
+    document.createElement('img'),
+    document.createElement('img'),
+    document.createElement('img'),
+    document.createElement('img')
+  ];
 
   const rightButton = document.createElement('div');
 
   carousel.appendChild(leftButton);
-  carousel.appendChild(image1);
-  carousel.appendChild(image2);
-  carousel.appendChild(image3);
-  carousel.appendChild(image4);
+  images.forEach(image => carousel.appendChild(image));
   carousel.appendChild(rightButton);
 
   carousel.classList.add('carousel');
@@ -34,15 +33,16 @@ function Carousel() {
   leftButton.textContent = '<';
   rightButton.textContent = '>';
 
-  image1.src = '../../assets/carousel/mountains.jpeg';
-  image2.src = '../../assets/carousel/computer.jpeg';
-  image3.src = '../../assets/carousel/trees.jpeg';
-  image4.src = '../../assets/carousel/turntable.jpeg';
+  images[0].src = '../../assets/carousel/mountains.jpeg';
+  images[1].src = '../../assets/carousel/computer.jpeg';
+  images[2].src = '../../assets/carousel/trees.jpeg';
+  images[3].src = '../../assets/carousel/turntable.jpeg';
 
-  image1.style.display = 'block';
-  
+  images[0].style.display = 'block';
+
   return carousel;
 }
+
 
 document.querySelector('.carousel-container').appendChild(Carousel());
 
